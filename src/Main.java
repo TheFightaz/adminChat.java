d50e0465375bfeed912ea06e6c4abf425ba637c3
TheFightaz <83822200+TheFightaz@users.noreply.github.com>
2021-05-10 12:40:46 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,23 @@
 
+package fightaz.adminchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import fightaz.adminchat.cmds.AdminChat;
+
+public class Main extends JavaPlugin {
+	
+	public static Main main;
+	
+	public void onEnable( ) {
+		
+		main = this;
+		
+		new AdminChat(this);
+	}
+
+	
+	public static Main getMain() {
+		return main;
+	}
+}
